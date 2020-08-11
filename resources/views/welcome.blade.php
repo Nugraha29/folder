36dc660330fdf7f79239d93fc0e1c2aae7ff36e9
Rizky Maulana <1606033@sttgarut.ac.id>
2020-08-11 22:47:29 +0700

    fix landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Creative - Start Bootstrap Theme</title>
+        <title>SISTEM INFORMASI PELAPORAN DAN PENGADUAN DINAS LINGKUNGAN HIDUP</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
@@ -15,6 +15,8 @@
         <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
         <!-- Third party plugin CSS-->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" rel="stylesheet" />
+        
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
     </head>
@@ -22,20 +24,24 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <a class="navbar-brand js-scroll-trigger" href="/">
                   <img src="assets/images/logo.png" width="5%"> Dinas Lingkungan Hidup
                 </a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/login">Pelaporan</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/pengaduan/create">Pengaduan</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger text-white" href="{{ url ('/login') }}">Pelaporan</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger text-white" href="{{ url ('/pengaduan/create') }}">Pengaduan</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
         <!-- Masthead-->
-        <header class="masthead">
+        <header>
+            <div class="overlay"></div>
+            <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+              <source src="assets/video/bg-720.mp4" type="video/mp4">
+            </video>
             <div class="container h-100">
                 <div class="row h-100 align-items-center justify-content-center text-center">
                     <div class="col-lg-10 align-self-end">
@@ -43,9 +49,12 @@
                         <hr class="divider my-4" />
                     </div>
                     <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 font-weight-light mb-0">Dinas Lingkungan Hidup</p>
+                        <p class="text-white-75 font-weight-light mb-0" style="font-size:25px">Dinas Lingkungan Hidup</p>
                         <p class="text-white-75 font-weight-light mb-0">Kabupaten Garut</p>
-                        <p class="text-white-75 font-weight-light mb-3">Jl. Terusan Pahlawan, Sukagalih, Kec. Tarogong Kidul, Kabupaten Garut, Jawa Barat 44151</p>
+                        <p class="text-white-75 font-weight-light mb-0" style="font-size:15px">Jl. Terusan Pahlawan, Sukagalih, Kec. Tarogong Kidul, Kabupaten Garut</p>
+                        <p class="text-white-75 font-weight-light mb-3" style="font-size:15px">Jawa Barat 44151</p>
+                        <a class="btn btn-primary btn-sm js-scroll-trigger" href="/"><i class="fa fa-arrow-left"></i> Kembali Ke Halaman Utama</a>
+
                     </div>
                 </div>
             </div>
